eaac915705a50305d3e05ebad287c5b44462c43d
abumalick <abdullah.hilson@gmail.com>
2020-05-05 15:34:10 +0000

    feat: add build folder to git

---
+++ build/precache-manifest.564b19da111d5c411d25dd977af091e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191b8ba34e8848bcb8dc77690b7f8d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65189200ca48cbe3272a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0af23f0de4c1736af78",
+    "url": "/static/js/2.f639d02d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f639d02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65189200ca48cbe3272a",
+    "url": "/static/js/main.75ff872d.chunk.js"
+  },
+  {
+    "revision": "fa2b592ef3db85615a55",
+    "url": "/static/js/runtime-main.642ee947.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
